6d9396cfcf82fc45770c2bc83b0f9b141227b20d
PrinceNorin <norin.hor@gmail.com>
2014-09-16 14:19:40 +0700

    Add homes controller and index action and make root_path to point to index action

---
+++ app/controllers/homes_controller.rb
@@ -0,0 +1,5 @@
+class HomesController < ApplicationController
+  def index
+    @message = 'Welcome to Ruby on Rails Deployment on Heroku'
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 FirstApp::Application.routes.draw do
+  root to: 'homes#index'
+  get '/homes' => 'homes#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
